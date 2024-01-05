4d6963c0cb3ed9085f71f7439e2c2f96ed42f50d
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-01-04 20:39:57 -0500

    Add Colors in tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        one: "#1f2024",
+        two: "#003b8e",
+        three: "#1564BF",
+        four: "#5e91f2",
+        five: "#92b8ff",
+      },
+    },
+  },
+  plugins: [],
+};
