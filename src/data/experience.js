33740eec45b6d915fb9d9feefa20750fe87e8c16
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-01-07 17:48:17 -0500

    Add Experence and ExperenceItem components

---
+++ src/data/experience.js
@@ -0,0 +1,74 @@
+export const data = [
+  {
+    empresa: "Movistar (Telefónica Hispam)",
+    actual: true,
+    duracion: "4 Años y 5 meses",
+    fechas: "Septiembre 2019 - Actualidad",
+    inicio: "Sept 2019",
+    cargos: [
+      {
+        cargo: "Especialista Desarrollo Fullstack",
+        duracion: "+3 Años",
+        descripcion:
+          "Desarrollo de aplicaciones web, móviles y backend, utilizando tecnologías como Angular, React, Ionic, Android, NodeJS, Java, Spring Boot, C#, PHP, Python. Base de datos Mysql, SQL SERVER, Oracle, Mysql, Postgres, MongoDB.",
+      },
+      {
+        cargo: "Devops Architect",
+        duracion: "6 meses",
+        descripcion:
+          "Creación y mantenimiento de pipelines de CI/CD utilizando herramientas como Jenkins, SonarQube, Docker, Kubernetes. Implementación de pipelines con Azure.",
+      },
+      {
+        cargo: "Profesional de Soporte y Evolución Tecnológica",
+        duracion: "6 meses",
+        descripcion:
+          "Desarrollo de aplicaciones web utilizando tecnologías como Javascript, Angular, C#, Base de datos SQL SERVER.",
+      },
+    ],
+  },
+  {
+    empresa: "Ipsos Colombia",
+    actual: false,
+    duracion: "1 Año",
+    fechas: "Septiembre 2018 - Septiembre 2019",
+    inicio: "Sept 2018",
+    cargos: [
+      {
+        cargo: "Software Application Developer",
+        duracion: "1 Año",
+        descripcion:
+          "Desarrollo de aplicaciones web utilizando tecnologías como Javascript, C#, Base de datos SQL SERVER.",
+      },
+    ],
+  },
+  {
+    empresa: "Freelancer",
+    actual: true,
+    duracion: "+5 Años",
+    fechas: "Septiembre 2017 - Actualidad",
+    inicio: "Sept 2017",
+    cargos: [
+      {
+        cargo: "Desarrollador Web",
+        duracion: "+5 Años",
+        descripcion:
+          "Desarrollo de aplicaciones web y móviles, utilizando tecnologías como Angular, React, Javascript, PHP, C#, Base de datos Mysql, SQL SERVER y MongoDB.",
+      },
+    ],
+  },
+  {
+    empresa: "Alphil Sistemas",
+    actual: false,
+    duracion: "+2 Años",
+    fechas: "Julio 2015 - Septiembre 2017",
+    inicio: "Jul 2015",
+    cargos: [
+      {
+        cargo: "Analista de sistema",
+        duracion: "+2 Años",
+        descripcion:
+          "Desarrollo de aplicaciones web utilizando tecnologías como Javascript, C#, Base de datos SQL SERVER.",
+      },
+    ],
+  },
+];
